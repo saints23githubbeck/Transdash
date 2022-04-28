6149a009f0999f79326ef9a930950755932d8def
unknown <juwonemmanuel22@gmail.com>
2022-04-28 12:43:57 -0700

    Second Commit

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const dropDown= () => {
         eachRightIcon.classList.toggle("hideIcon")
         eachDownIcon.classList.toggle("showIcon")
        
-
     })
 
         console.log(content, eachSideBar)
@@ -29,3 +28,59 @@ const dropDown= () => {
 }
 
 dropDown()
+
+
+ if(location.pathname === "/packagesToConsolidated.html"){
+    const modal= document.querySelector(".searchShipmentModal")
+    const searchBtn = document.querySelector(".consolidation-search-shipment")
+    const removeModal = document.querySelector(".removeModal")
+
+    searchBtn.addEventListener("click" , (e) => {
+        e.preventDefault()
+
+        modal.style.display="block"
+    })
+
+    removeModal.addEventListener("click" , (e) => {
+        e.preventDefault()
+
+        modal.style.display="none"
+    })
+
+    const consolidateModal = document.querySelector(".consolidateModal")
+    const ok = document.querySelector(".ok")
+    const consolidatBtn = document.querySelector(".consolidateBtn")
+
+    consolidatBtn.addEventListener("click" , (e) => {
+        e.preventDefault()
+
+        consolidateModal.style.display="block";
+    })
+
+    ok.addEventListener("click" , (e) => {
+        e.preventDefault()
+
+        consolidateModal.style.display="none";
+    })
+ }
+
+
+
+if (location.pathname==="/packageLists.html" || location.pathname ==="/listOfPreAlert.html"){
+
+    const optionBtn = document.querySelectorAll(".option")
+    const dropOption = document.querySelectorAll(".dropOption")
+
+
+    optionBtn.forEach((button, index) => {
+
+        const eachOption = dropOption[index]
+
+        button.addEventListener("click" , (e) => {
+            e.preventDefault()
+            eachOption.classList.toggle("showOption")
+            button.classList.toggle("toggleBackground")
+        })
+    })
+   
+}
